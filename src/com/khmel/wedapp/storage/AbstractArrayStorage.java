1986120814932bbec25e65a5f7e7b57a28cc4de9
Khmel19 <khmialinka@gmail.com>
2020-04-23 03:46:51 +0300

    update AbstractArrayStorage, ArrayStorage, SortedArrayStorage

--- src/com/khmel/wedapp/storage/AbstractArrayStorage.java
+++ src/com/khmel/wedapp/storage/AbstractArrayStorage.java
@@ -2,11 +2,52 @@ package com.khmel.wedapp.storage;
 
 import com.khmel.wedapp.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
-
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume resume) {
+        if (getIndex(resume.getUuid()) < 0) {
+            System.out.println("Resume " + resume.getUuid() + " not exist");
+        } else {
+            storage[getIndex(resume.getUuid())] = resume;
+        }
+    }
+
+    public void save(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            System.out.println("Resume " + resume.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            insertElement(resume, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+           fillDeletedElement(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
 
     public int size() {
         return size;
@@ -20,6 +61,10 @@ public abstract class AbstractArrayStorage implements Storage {
         }
         return storage[index];
     }
+
+    protected abstract void fillDeletedElement(int index);
+
+    protected abstract void insertElement(Resume resume, int index);
 
     protected abstract int getIndex(String uuid);
 }
--- src/com/khmel/wedapp/storage/ArrayStorage.java
+++ src/com/khmel/wedapp/storage/ArrayStorage.java
@@ -2,61 +2,19 @@ package com.khmel.wedapp.storage;
 
 import com.khmel.wedapp.model.Resume;
 
-import java.util.Arrays;
-
-public class ArrayStorage extends AbstractArrayStorage{
+public class ArrayStorage extends AbstractArrayStorage {
     private static final int STORAGE_LIMIT = 10000;
     private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1) {
-            System.out.println("Resume " + resume.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = resume;
-            size++;
-        }
-    }
-
-    public void update(Resume resume) {
-        if (getIndex(resume.getUuid()) == -1) {
-            System.out.println("Resume " + resume.getUuid() + " not exist");
-        } else {
-            storage[getIndex(resume.getUuid())] = resume;
-        }
-    }
-
-    public Resume get(String uuid) {
-        if (getIndex(uuid) == -1) {
-            System.out.println("Resume " + uuid + " not exist");
-            return null;
-        }
-        return storage[getIndex(uuid)];
-    }
-
-    public void delete(String uuid) {
-        if (getIndex(uuid) == -1) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            storage[getIndex(uuid)] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
-    public int size() {
-        return size;
+    @Override
+    protected void insertElement(Resume resume, int index) {
+        storage[size] = resume;
     }
 
     @Override
